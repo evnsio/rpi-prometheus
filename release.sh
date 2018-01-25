5b14e370f1208151b5fe1ef2eb838bc6ca305832
Chris Evans <chris@evns.io>
2018-01-25 12:20:40 +0000

    Improved release script

--- release.sh
+++ release.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 # Don't accidentally build on a non arm architecture
 ARCH=`uname -m`
 if [ "$ARCH" != "armv7l" ]; then
@@ -10,6 +12,10 @@ fi
 # The docker build pulls from master so the versions should align
 VERSION=`curl -s https://raw.githubusercontent.com/prometheus/prometheus/master/VERSION`
 
-# Build, tag and push the Docker image
-docker build -t evns/rpi-prometheus:v${VERSION} .
+# Build and push the Docker image
+docker build -t evns/rpi-prometheus .
+docker push evns/rpi-prometheus
+
+# Tag the version and push
+docker tag evns/rpi-prometheus evns/rpi-prometheus:v${VERSION}
 docker push evns/rpi-prometheus:v${VERSION}
