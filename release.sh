3a27e55639067bbc2c3fcae6073e1e67eddcf714
Chris Evans <chris@evns.io>
2018-01-25 10:47:38 +0000

    Release script now only runs on arm and auto versions

--- release.sh
+++ release.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
-docker build -t evns/rpi-prometheus:v2.1.0 .
-docker push evns/rpi-prometheus:v2.1.0
+
+# Don't accidentally build on a non arm architecture
+ARCH=`uname -m`
+if [ "$ARCH" != "armv7l" ]; then
+  echo "Only run this on an Arm processor!"
+  exit 1
+fi
+
+# The docker build pulls from master so the versions should align
+VERSION=`curl -s https://raw.githubusercontent.com/prometheus/prometheus/master/VERSION`
+
+# Build, tag and push the Docker image
+docker build -t evns/rpi-prometheus:v${VERSION} .
+docker push evns/rpi-prometheus:v${VERSION}
